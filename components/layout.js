49d022ec4580e9b7aa974d74a0d202f6e9069596
Bryan Rojas <bryroj14@gmail.com>
2019-05-09 05:52:36 -0700

    Updated packages

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>Bryan Rojas - Personal Portfolio</title>
-            <link rel="stylesheet" href="https://bootswatch.com/4/cyborg/bootstrap.min.css"/>
+            <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css"/>
         </Head>
         <Navbar/>
         <div className="container">
